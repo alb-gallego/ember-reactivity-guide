ce95cb251cf1502a83a383d3be1722a30053bf9e
Alberto <albertogh1571@gmail.com>
2025-03-21 14:54:11 +0100

    Initialize project

---
+++ app/controllers/ember-concurrency.ts
@@ -0,0 +1,3 @@
+import Controller from '@ember/controller';
+
+export default class EmberConcurrencyController extends Controller {}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,10 @@
 'use strict';
-
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    'ember-cli-babel': { enableTypeScriptTransform: true },
-
     // Add options here
   });
-
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
     skipBabel: [
@@ -16,5 +12,17 @@ module.exports = function (defaults) {
         package: 'qunit',
       },
     ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: ['postcss-loader'],
+            },
+          ],
+        },
+      },
+    },
   });
 };
